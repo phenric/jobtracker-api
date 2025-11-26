dbd35d544c9e4aae24bede254300ec7334a2c924
phenric <phenric.callewaert@gmail.com>
2025-11-26 11:10:24 +0100

    Init

---
+++ src/main/java/com/vedisee/jobstracker/JobstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vedisee.jobstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobstrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobstrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedisee/jobstracker/JobstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedisee.jobstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
